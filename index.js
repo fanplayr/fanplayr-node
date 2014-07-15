a20c8b9cfcdb593ecf62f19eb015d0f4cfea9686
Matt Benton <matt@mattbenton.net>
2014-07-15 14:54:43 -0700

    Added lodash

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 #!/usr/bin/env node
 
+var fs = require("fs");
+var path = require("path");
+var _ = require("lodash");
+
 function getUserHome () {
   return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
 
 var fanplayr = {};
 
-var aws = fanplayr.aws = {};
+var aws = fanplayr.aws = {
+  credentials: {}
+};
 
-console.log(getUserHome());
+var aws_file = process.env['FP_AWS_JSON'];
+if ( aws_file && fs.existsSync(aws_file) ) {
+  var data = JSON.parse(fs.readFileSync(aws_file, { encoding: 'utf8'}));
+  if ( data.credentials ) {
+    _.forEach(data.credentials, function ( obj, name ) {
+      aws.credentials[name] = {
+        key: obj.key,
+        secret: obj.secret
+      };
+    });
+  }
+}
 
 module.exports = fanplayr;
---
+++ test.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+var fanplayr = require("./index");
+
